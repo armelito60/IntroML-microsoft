724490410fe595055ab62fac1e019bb95c05fda3
armelito60 <tchiasso@et.esiea.fr>
2021-08-03 18:07:13 +0200

    installation des modules scikit-learn matplotlib

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print('Hello, world')
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model, model_selection
+
+X, y = datasets.load_diabetes(return_X_y=True)
+print(X.shape)
+print(X[0])
